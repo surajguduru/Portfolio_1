ce6a9c3f5d42028151078b0542a9a587c8235ffe
surajguduru <guduru.23bcs10012@ms.sst.scaler.com>
2024-06-25 22:20:01 +0530

    Added styling in about, portfolio and interests section

--- src/Interests.js
+++ src/Interests.js
@@ -8,33 +8,33 @@ function Interests() {
                 <h2>Interests</h2>
                 <div className="interests-cards-container">
                     <div className="interests-card">
-                        <div className="card">
-                            <i className="zmdi zmdi-tv font-orangered interest-icon"></i>
-                            <h3 className="card_title">Movie</h3>
+                        <div className="interests-card-box">
+                            <i className="zmdi zmdi-tv font-orangered interests-icon"></i>
+                            <h3 className="interests-card-title">Movie</h3>
                         </div>
                     </div>
                     <div className="interests-card">
-                        <div className="card">
-                            <i className="zmdi zmdi-laptop font-orangered interest-icon"></i>
-                            <h3 className="card_title">Coding</h3>
+                        <div className="interests-card-box">
+                            <i className="zmdi zmdi-laptop font-orangered interests-icon"></i>
+                            <h3 className="interests-card-title">Coding</h3>
                         </div>
                     </div>
                     <div className="interests-card">
-                        <div className="card">
-                            <i className="zmdi zmdi-radio font-orangered interest-icon"></i>
-                            <h3 className="card_title">Music</h3>
+                        <div className="interests-card-box">
+                            <i className="zmdi zmdi-radio font-orangered interests-icon"></i>
+                            <h3 className="interests-card-title">Music</h3>
                         </div>
                     </div>
                     <div className="interests-card">
-                        <div className="card">
-                            <i className="zmdi zmdi-smartphone font-orangered interest-icon"></i>
-                            <h3 className="card_title">Gaming</h3>
+                        <div className="interests-card-box">
+                            <i className="zmdi zmdi-smartphone font-orangered interests-icon"></i>
+                            <h3 className="interests-card-title">Gaming</h3>
                         </div>
                     </div>
                     <div className="interests-card">
-                        <div className="card">
-                            <i className="zmdi zmdi-library font-orangered interest-icon"></i>
-                            <h3 className="card_title">Reading</h3>
+                        <div className="interests-card-box">
+                            <i className="zmdi zmdi-library font-orangered interests-icon"></i>
+                            <h3 className="interests-card-title">Reading</h3>
                         </div>
                     </div>
                 </div>
